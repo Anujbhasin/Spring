976f3e836b1b110e62bc1a5a1ef2077f5e7c283e
Anuj Bhasin <54930626+Anujbhasin@users.noreply.github.com>
2024-06-03 20:02:03 +0530

    Add files via upload

---
+++ example46/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eazyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableFeignClients(basePackages="com.eazybytes.eazyschool.proxy")
+public class EazyschoolApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyschoolApplication.class, args);
+	}
+
+}
